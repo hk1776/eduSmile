5800c26a7c60c8b2344735791a20c207b8670801
egoing <dannys982355@gmail.com>
2025-01-08 10:35:36 +0900

    Add ai_models folder and initial project setup

---
+++ src/main/java/com/example/edusmile/EdusmileApplication.java
@@ -0,0 +1,13 @@
+package com.example.edusmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusmileApplication.class, args);
+	}
+
+}
