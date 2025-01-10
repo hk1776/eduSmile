e9d5a3cacbc7dbfbcd7e132c8992bd5b6704e2f9
kim_daekil_v1 <gg7271@naver.com>
2025-01-10 13:22:24 +0900

    kim daekil v1

--- src/main/java/com/example/edusmile/Controller/LoginController.java
+++ src/main/java/com/example/edusmile/Controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.edusmile.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -21,6 +22,12 @@ public class LoginController {
 
     @GetMapping("user/signup")
     public String signup() {
+
+        return "signup";
+    }
+
+    @PostMapping("user/signupsave")
+    public String signupsave() {
 
         return "signup";
     }
--- src/main/java/com/example/edusmile/Dto/MemberDto.java
+++ src/main/java/com/example/edusmile/Dto/MemberDto.java
@@ -1,4 +1,32 @@
 package com.example.edusmile.Dto;
 
+import com.example.edusmile.Entity.MemberEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.security.Timestamp;
+
 public class MemberDto {
+
+
+    private Long id;
+    private String loginId;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String school;
+    private String schoolClass;
+    private String role;
+    private String teacherCode;
+    private Timestamp createDate;
+    private Timestamp updateDate;
+
+    public MemberEntity toEntity() {
+        return new MemberEntity(null,loginId,password,name,phoneNumber,school,schoolClass,
+                role,teacherCode,null,null);
+    }
 }
