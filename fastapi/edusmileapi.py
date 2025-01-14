06467c0acc3dcb1b09682a99e609e8bbe8077c1f
kim <candy1119@naver.com>
2025-01-14 16:35:53 +0900

    fastapi추가

---
+++ fastapi/edusmileapi.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import speech_recognition as sr
+import tempfile
+
+
+app = FastAPI()
+
+@app.get("/edusmile")
+async def get_edusmile(file: UploadFile = File(...)):
+    classsummary,notice,task,test=1,1,1,1
+    return {"수업요약":classsummary,"공지사항": notice, "과제/준비물": task,"시험": test}
+
