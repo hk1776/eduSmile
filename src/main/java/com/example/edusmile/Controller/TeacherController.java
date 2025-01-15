025d6e826b8f801318a7f246e57a808952dd799d
hk1776 <hk1776@naver.com>
2025-01-15 17:28:46 +0900

    index update

--- src/main/java/com/example/edusmile/Controller/TeacherController.java
+++ src/main/java/com/example/edusmile/Controller/TeacherController.java
@@ -1,26 +1,16 @@
 package com.example.edusmile.Controller;
-
-
-import com.example.edusmile.Dto.Classification;
 import com.example.edusmile.Entity.MemberEntity;
 import com.example.edusmile.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
