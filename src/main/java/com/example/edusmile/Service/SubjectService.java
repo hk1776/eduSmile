5ad4e02026ee527b6c33fdf2cbfa1c0a6263dcff
hk1776 <hk1776@naver.com>
2025-01-23 17:48:50 +0900

    수업 추가

--- src/main/java/com/example/edusmile/Service/SubjectService.java
+++ src/main/java/com/example/edusmile/Service/SubjectService.java
@@ -2,12 +2,15 @@ package com.example.edusmile.Service;
 
 import com.example.edusmile.Entity.Subject;
 import com.example.edusmile.Repository.SubjectRepository;
+import jakarta.persistence.Column;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -19,5 +22,16 @@ public class SubjectService {
     }
     public Optional<Subject> findById(String id) {
         return subjectRepository.findById(id);
+    }
+    public Subject save(Long memberId, String subjectInput,int grade,String divClass) {
+        Subject subject = new Subject();
+        subject.setSubject(subjectInput);
+        subject.setMemberId(memberId);
+        subject.setGrade(grade);
+        subject.setDivClass(divClass);
+        String uuid = UUID.randomUUID().toString();
+        String id = uuid.replace("-", "").substring(0, 10);
+        subject.setId(id);
+        return subjectRepository.save(subject);
     }
 }
